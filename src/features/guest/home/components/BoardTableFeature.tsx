e492c3e320932279b8cedf7316fa97bd676d07e5
Andre <95618374+andrecausingg@users.noreply.github.com>
2026-03-25 12:32:26 +0800

    remove test modal

--- src/features/guest/home/components/BoardTableFeature.tsx
+++ src/features/guest/home/components/BoardTableFeature.tsx
@@ -9,8 +9,6 @@ import { selectTodoDragAndDrop } from "../../../../redux/features/board/todoBoar
 
 // Custom Hook
 import { useDragAndDrop } from "../../../../hooks/board/useDragAndDrop";
-import { useState } from "react";
-import ModalForm from "../../../../component/form/ModalFormComponent";
 
 const BoardTable: React.FC = () => {
   // Redux | state destructor
@@ -18,77 +16,6 @@ const BoardTable: React.FC = () => {
 
   // Custom Hook | destructor
   const { allowDrop, onDragStart, onDrop } = useDragAndDrop();
-
-  const [opened, setOpened] = useState(false);
-
-  const loginFormConfig = [
-    {
-      label: "Email",
-      api_key: "email",
-      tag: "TextInput",
-      required: true,
-    },
-    {
-      label: "Password",
-      api_key: "password",
-      tag: "PasswordInput",
-      required: true,
-    },
-  ];
-
-  const registerFormConfig = [
-    {
-      label: "Email",
-      api_key: "email",
-      tag: "TextInput",
-      required: true,
-    },
-    {
-      label: "Password",
-      api_key: "password",
-      tag: "PasswordInput",
-      required: true,
-    },
-    {
-      label: "Password Confirmation",
-      api_key: "password_confirmation",
-      tag: "PasswordInput",
-      required: true,
-    },
-  ];
-
-  const todoFormConfig = [
-    {
-      label: "Title",
-      api_key: "title",
-      tag: "TextInput",
-      required: true,
-    },
-    {
-      label: "Description",
-      api_key: "description",
-      tag: "Textarea",
-      required: true,
-    },
-    {
-      label: "Expired at",
-      api_key: "expired_at",
-      tag: "DateInput",
-      required: true,
-    },
-  ];
-
-  const networkConfig = {
-    api: "http://localhost:8000/api/users",
-    method: "POST",
-    mutation: null,
-  };
-
-  const modalConfig = {
-    opened,
-    onClose: () => setOpened(false),
-    title: "Login Form",
-  };
 
   return (
     <div>
@@ -104,12 +31,6 @@ const BoardTable: React.FC = () => {
           />
         ))}
       </div>
-
-      <ModalForm
-        formConfig={todoFormConfig}
-        networkConfig={networkConfig}
-        modalConfig={modalConfig}
-      />
     </div>
   );
 };
